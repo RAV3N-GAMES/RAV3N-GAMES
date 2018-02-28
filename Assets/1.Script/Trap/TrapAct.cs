b2c5ada9e604586186303cf1d8dfb0d57c06f152
DESKTOP-3LQMLPV\user <joghun93@naver.com>
2018-02-28 20:21:40 +0900

    update library

---
+++ Assets/1.Script/Trap/TrapAct.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapAct : MonoBehaviour {
+    Animator Ani_Trap;
+    public static bool isCatch;
+
+	// Use this for initialization
+	void Start () {
+        Ani_Trap = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.tag.Equals("Enemy"))
+        {
+            //물린 적에게 데미지(필요하면?)
+            
+        }
+    }
+}
