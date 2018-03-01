0fd14b96d581ddba36957fbefd5343b23969626b
DESKTOP-3LQMLPV\user <joghun93@naver.com>
2018-03-01 15:29:57 +0900

    deleted Temp folder

---
+++ Assets/1.Script/Trap/HumanTrapAct.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HumanTrapAct : MonoBehaviour {
+    Animator Ani_Trap;
+    // Use this for initialization
+    void Start () {
+        Ani_Trap = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag.Equals("Enemy"))
+        {
+            //물린 적에게 데미지(필요하면?)
+        }
+    }
+}
