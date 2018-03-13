f941db8c87dcbb2b36f555cf039fa34964a0a5a8
DESKTOP-3LQMLPV\user <joghun93@naver.com>
2018-03-13 17:40:43 +0900

    우철이꺼 스크립트 들고와서 벽 작업 마무리 예정

--- Assets/1.Script/Wall/WallActs.cs
+++ Assets/1.Script/Wall/WallActs.cs
@@ -9,6 +9,7 @@ public class WallActs : MonoBehaviour {
     // Use this for initialization
     void Awake() {
         thisObject = GetComponent<ObjectInfo>();
+        initBuilding(thisObject);
     }
 
     // Update is called once per frame
@@ -25,5 +26,21 @@ public class WallActs : MonoBehaviour {
         DisplayObject thisDisplay = GetComponent<DisplayObject>();
         thisDisplay.DestroyObj();
         Destroy(this.gameObject);
+    }
+
+    void initBuilding(ObjectInfo info) {
+        switch (info.id)
+        {
+            case "FunctionalBuilding":
+                //6x6반경에 있는 TrapActs에 bool type isSafe 생성 -> 이를 true로 변경
+                //6x6 반경은 콜라이더로 해결.
+
+                break;
+            case "CoreBuilding":
+                //올라온 원거리캐릭사거리 증가
+                //원거리캐릭 인지범위 늘리는걸로 해결
+
+                break;
+        }
     }
 }
