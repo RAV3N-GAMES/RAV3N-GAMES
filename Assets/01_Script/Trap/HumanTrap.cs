899f2e1e639f21585dc142c2483d7020ad992175
DESKTOP-3LQMLPV\user <joghun93@naver.com>
2018-03-23 16:03:27 +0900

    add traps

---
+++ Assets/01_Script/Trap/HumanTrap.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HumanTrap : Trap {
+    double EffectContinuousTime;
+    public double CoolTime;
+    public bool isCool;
+
+    // Use this for initialization
+	void Start () {
+        //checkCount(0);
+        Data_Player.Fame = 4;
+        TrapManager.TrapCount[0]++;
+        TrapAni = GetComponent<Animator>();
+        CoolTime = TrapManager.Tbl_TrapSetup[Data_Player.Fame-4].CoolTime;
+        EffectContinuousTime = TrapManager.Tbl_TrapSetup[Data_Player.Fame - 4].EffectContinuousTime;
+        isCool = false;
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+    
+    public override void Acts(Collider col) {
+        //Trap 클래스의 on collision 함수에서 Enemy 태그를 가진 오브젝트가 콜라이더에 닿으면 실행. 
+        //쿨타임 적용
+        if(!isCool)
+            StartCoroutine(CatchStatus(EffectContinuousTime, col));
+    }
+
+    IEnumerator CatchStatus(double EffectContinuousTime, Collider col) {
+        TrapAni.Play("Trap", -1, 0.5f);//중간(잡는 모션) 실행
+        TrapAni.speed = 0;//바로 정지
+        CatchEnemy(col);
+        yield return new WaitForSeconds((float)EffectContinuousTime);
+        TrapAni.speed = 1;
+        FreeEnemy(col);
+        isCool = true;
+        yield return StartCoroutine(CoolTimeDelay(CoolTime));
+    }
+
+    IEnumerator CoolTimeDelay(double CoolTime) {
+        yield return new WaitForSeconds((float)CoolTime);
+        isCool = false;
+    }
+
+    void CatchEnemy(Collider col) {//포획 대상 정지
+        Rigidbody rigid = col.gameObject.GetComponent<Rigidbody>();
+        rigid.constraints = RigidbodyConstraints.FreezeAll;
+    }
+    void FreeEnemy(Collider col) {//포획 대상 풀어줌
+        Rigidbody rigid = col.gameObject.GetComponent<Rigidbody>();
+        rigid.constraints = RigidbodyConstraints.None;
+        rigid.constraints = RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezeRotation;
+    }
+
+}
